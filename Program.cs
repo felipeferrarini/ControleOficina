77a5631c31799b20ce94ead4c269395e5d3f600d
Felipe de Abreu Ferrarini <ferrarinifelipe@gmail.com>
2020-05-07 20:58:38 -0300

    asd

--- Program.cs
+++ Program.cs
@@ -7,6 +7,7 @@ namespace ControleOficina
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello World!");
         }
     }
 }
